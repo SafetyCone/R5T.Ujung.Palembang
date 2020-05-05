dd3bf3e8309c11c5aeb36fb6aaf1dcfb0e133c28
MinexAutomation <minex.automation@gmail.com>
2020-05-05 16:37:17 -0700

    Added a few service implementations to start.

---
+++ source/R5T.Ujung.Palembang/Code/Services/Implementations/BuildConfigurationDirectoryNameProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.Palembang;
+
+
+namespace R5T.Ujung.Palembang
+{
+    /// <summary>
+    /// A <see cref="IBuildConfigurationDirectoryNameProvider"/> implementation using the <see cref="IBuildConfigurationNameProvider"/> service that presumes that the build configuration directory name is the same as the build configuration name.
+    /// </summary>
+    public class BuildConfigurationDirectoryNameProvider : IBuildConfigurationDirectoryNameProvider
+    {
+        private IBuildConfigurationNameProvider BuildConfigurationNameProvider { get; }
+
+
+        public BuildConfigurationDirectoryNameProvider(IBuildConfigurationNameProvider buildConfigurationNameProvider)
+        {
+            this.BuildConfigurationNameProvider = buildConfigurationNameProvider;
+        }
+
+        public string GetBuildConfigurationDirectoryName()
+        {
+            var buildConfigurationName = this.BuildConfigurationNameProvider.GetBuildConfigurationName();
+
+            var buildConfigurationDirectoryName = buildConfigurationName; // Presume the build configuration directory name is the same as the build configuration name.
+            return buildConfigurationDirectoryName;
+        }
+    }
+}
---
+++ source/R5T.Ujung.Palembang/Code/Services/Implementations/ProjectDirectoryNameProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.Palembang;
+
+
+namespace R5T.Ujung.Palembang
+{
+    /// <summary>
+    /// The default <see cref="IProjectDirectoryNameProvider"/> implementation using the <see cref="IProjectNameProvider"/> service and presumes that the project directory name is the same as the project name.
+    /// </summary>
+    public class ProjectDirectoryNameProvider : IProjectDirectoryNameProvider
+    {
+        private IProjectNameProvider ProjectNameProvider { get; }
+
+
+        public ProjectDirectoryNameProvider(IProjectNameProvider projectNameProvider)
+        {
+            this.ProjectNameProvider = projectNameProvider;
+        }
+
+        public string GetProjectDirectoryName()
+        {
+            var projectName = this.ProjectNameProvider.GetProjectName();
+
+            var projectDirectoryName = projectName; // Presume the project directory name is the same as the project name.
+            return projectDirectoryName;
+        }
+    }
+}
